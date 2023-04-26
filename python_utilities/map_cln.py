d6ff2b9af2e00d3a6b26b1ea3c97363ce2375d2d
Andrew Calderwood, EIT <44653082+andrewcalderwood@users.noreply.github.com>
2023-04-26 06:10:30 -0700

    Update folder structure and work on stream-aquifer model

--- python_utilities/map_cln.py
+++ python_utilities/map_cln.py
@@ -6,12 +6,37 @@ Author: Andrew Calderwood
 """
 
 import matplotlib.pyplot as plt
+from shapely.geometry import Polygon
 import geopandas as gpd
+import pandas as pd
+
+def gdf_bnds(gdf, ax, buf=1):
+    """ Use the x-y bounds of a geodataframe to set the axes limits on a plot"""
+    gdf_bnd = gpd.GeoDataFrame(pd.DataFrame([0]), geometry = [gdf.unary_union.buffer(buf)], crs=gdf.crs)
+    minx, miny, maxx, maxy = gdf_bnd.bounds.values[0]
+    ax.set_xlim(minx, maxx)
+    ax.set_ylim(miny, maxy)
+    return(gdf_bnd)
 
 
-def gdf_2_lims(gdf,ax):
-	""" Use the x-y bounds of a geodataframe to set the axes limits on a plot"""
-	xmin, ymin = gdf.geometry.bounds.loc[:,['minx','miny']].min()
-	xmax, ymax = gdf.geometry.bounds.loc[:,['maxx','maxy']].max()
-	ax.set_xlim(xmin,xmax)
-	ax.set_ylim(ymin,ymax)
+def pnt_2_tup(pnt):
+    """simple function to get tuple from geopandas/shapely Point"""
+    return(list(pnt[0].coords)[0])
+
+def lab_pnt(geom, scale = None, xscale = None, yscale = None):
+    """create an x,y coordinate tuple  needed for labeling a plot"""
+    xy = pnt_2_tup(geom.geometry.representative_point())
+    if xscale != None:
+        xy = tuple([p * s for p, s in zip(xy, (xscale, yscale))])
+    elif scale != None:
+        xy = tuple(map((scale).__mul__, xy ))
+    return(xy)
+
+def plt_cln(ax):
+    """ Set basic xy axis labels, reduce axis clutter"""
+    ax.ticklabel_format(style='plain')
+    plt.ylabel('Easting (m)')
+    plt.xlabel('Northing (m)')
+    
+    plt.locator_params(axis='x', nbins=5)
+    plt.locator_params(axis='y', nbins=6)
---
+++ python_utilities/mf_utility.py
@@ -0,0 +1,100 @@
+"""
+mf_utility module. 
+Different functions for modflow set up with general python functions
+First iteration as a Module April 2023
+Author: Andrew Calderwood
+"""
+
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+
+def get_layer_from_elev(elev, botm_slice, nlay):
+    """  Return uppermost model layer (0-based) occupied at least partly by some elevation data
+    Parameters
+    ----------
+    elev: 1D array (n) with elevations matching model elevation units
+    botm: 2D array (nlay, n) with layer elevations of model using same x,y locations at elev1D
+    """
+    elev_lay = np.zeros(len(elev))
+    for k in np.arange(0,nlay-1):
+        for j in np.arange(0,len(elev)):
+            if botm_slice[k,j] > elev[j]:
+                elev_lay[j] = k + 1
+    return(elev_lay.astype(int))
+    
+def ghb_df(rows, cols, ghb_hd, distance):
+    """ Given rows and columns create GHB based on interpolated head levels
+    INPUT:
+    rows, cols are 0 based row,column
+    ghb_hd is a pd.Series with an index of 0-based row,cols 
+    distance is the GHB distnace used for all cells
+    OUTPUT:
+    dataframe with the 0 based layer, row, column, boundary head and conductance"""
+    # pull out head for rows and columns
+    head = ghb_hd.loc[list(zip(rows, cols))].value.values
+    ghb_lay = get_layer_from_elev(head, botm[:,rows, cols], m.dis.nlay)
+
+    df = pd.DataFrame(np.zeros((np.sum(nlay - ghb_lay),5)))
+    df.columns = ['k','i','j','bhead','cond']
+    # get all of the i, j,k indices to reduce math done in the for loop
+    n=0
+    nk = -1
+    for i, j in list(zip(rows,cols)):
+        nk +=1
+        for k in np.arange(ghb_lay[nk], nlay):
+            df.loc[n,'i'] = i
+            df.loc[n,'j'] = j
+            df.loc[n,'k'] = k
+            n+=1
+    df[['k','i','j']] = df[['k','i','j']].astype(int)
+    cond = hk[df.k, df.i, df.j]*(top_botm[df.k, df.i, df.j]-top_botm[df.k +1 , df.i, df.j])*delr/distance
+    df.cond = cond
+    df.bhead = ghb_hd.loc[list(zip(df.i, df.j))].value.values
+    # drop cells where the head is below the deepest cell?
+    return(df)
+    
+    
+def read_gage(gagenam):
+    """Load the Lake Gage file """
+    gage = pd.read_csv(gagenam, skiprows=1, delimiter = r'\s+', engine='python')
+    cols = gage.columns[1:-1]
+    gage = gage.dropna(axis=1)
+    gage.columns = cols
+    strt_date = pd.to_datetime(m.dis.start_datetime)
+    gage['dt'] = strt_date+(gage.Time*24).astype('timedelta64[h]')
+    gage = gage.set_index('dt')
+    gage['dVolume'] = gage.Volume.diff()
+    gage['Total_In'] = gage[['Precip.','Runoff','GW-Inflw','SW-Inflw']].sum(axis=1)
+    gage['Total_Out'] = gage[['Evap.','Withdrawal','GW-Outflw','SW-Outflw']].sum(axis=1)
+    gage['In-Out'] = gage.Total_In - gage.Total_Out
+    return(gage)
+    
+    
+def clean_sfr_df(model_ws, pd_sfr=None):
+    """Load sfr.out file and create new columns """
+    sfrout = flopy.utils.SfrFile(join(model_ws, m.name+'.sfr.out'))
+    sfrdf = sfrout.get_dataframe()
+    sfrdf = sfrdf.join(dt_ref.set_index('kstpkper'), on='kstpkper').set_index('dt')
+    # convert from sub-daily to daily using mean, lose kstpkper
+    sfrdf = sfrdf.groupby('segment').resample('D').mean(numeric_only=True)
+    sfrdf = sfrdf.reset_index('segment', drop=True)
+    sfrdf[['row','column']]-=1 # convert to python
+    cmd2cfs = 1/((0.3048**3)*86400) # cubic meters per day to cfs
+    sfrdf['month'] = sfrdf.index.month
+    sfrdf['WY'] = sfrdf.index.year
+    sfrdf.loc[sfrdf.month>=10, 'WY'] +=1
+    # add column to track days with flow
+    sfrdf['flowing'] = 1
+    sfrdf.loc[sfrdf.Qout <= 0, 'flowing'] = 0
+    if pd_sfr != None:
+        sfrdf = sfrdf.join(pd_sfr ,on=['segment','reach'],how='inner',lsuffix='_all')
+
+    # create different column for stream losing vs gaining seeapge
+    sfrdf['Qrech'] = np.where(sfrdf.Qaquifer>0, sfrdf.Qaquifer,0)
+    sfrdf['Qbase'] = np.where(sfrdf.Qaquifer<0, sfrdf.Qaquifer*-1,0 )
+    # booleans for plotting
+    sfrdf['gaining'] = (sfrdf.gradient == 0)
+    sfrdf['losing'] = (sfrdf.gradient >= 0)
+    sfrdf['connected'] = (sfrdf.gradient < 1)
+    return(sfrdf)
